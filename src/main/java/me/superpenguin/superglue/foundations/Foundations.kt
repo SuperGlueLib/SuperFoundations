271e6678d8eb5bf599f88bde717935e7dfe67d78
MLGPenguin <baww123@gmail.com>
2023-08-26 11:30:50 +1000

    Update toColor usages

--- src/main/java/me/superpenguin/superglue/foundations/Foundations.kt
+++ src/main/java/me/superpenguin/superglue/foundations/Foundations.kt
@@ -14,7 +14,7 @@ import org.bukkit.plugin.java.JavaPlugin
 import java.io.File
 
 /** Send a coloured message to a player */
-fun CommandSender.send(msg: String, hex: Boolean = false) = sendMessage(msg.toColour(hex))
+fun CommandSender.send(msg: String, hex: Boolean = false) = sendMessage(msg.toColor(hex))
 
 // A bunch of useful string methods.
 fun String.toIntOrElse(default: Int): Int = this.toIntOrNull() ?: default
