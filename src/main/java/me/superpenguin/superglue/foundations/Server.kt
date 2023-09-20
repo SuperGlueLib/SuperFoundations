59166aaaa61394cdb36851b48b168b4ffc7e7d72
MLGPenguin <baww123@gmail.com>
2023-09-20 13:41:21 +1000

    + Server.isFolia method

--- src/main/java/me/superpenguin/superglue/foundations/Server.kt
+++ src/main/java/me/superpenguin/superglue/foundations/Server.kt
@@ -4,5 +4,6 @@ object Server {
 
     /** @return true if the server is running paper, or a fork of paper. */
     fun isPaper() = runCatching { Class.forName("com.destroystokyo.paper.event.player.PlayerArmorChangeEvent") }.isSuccess
+    fun isFolia() = runCatching { Class.forName("io.papermc.paper.threadedregions.RegionizedServer") }.isSuccess
 
 }
