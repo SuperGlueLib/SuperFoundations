77df5f6aac59189169972ad8a02cf08b1cd5c41b
MLGPenguin <baww123@gmail.com>
2024-02-03 16:53:37 +1000

    + a number of shorter methods to retrieve the block pos from blocks, locations, etc.

--- src/main/java/com/github/supergluelib/foundation/misc/BlockPos.kt
+++ src/main/java/com/github/supergluelib/foundation/misc/BlockPos.kt
@@ -2,6 +2,12 @@ package com.github.supergluelib.foundation.misc
 
 import org.bukkit.Location
 import org.bukkit.World
+import org.bukkit.block.Block
+import org.bukkit.block.BlockState
+
+val Location.blockPos get() = BlockPos(blockX, blockY, blockZ)
+val Block.blockPos get() = location.blockPos
+val BlockState.blockPos get() = location.blockPos
 
 data class BlockPos(val x: Int, val y: Int, val z: Int) {
     fun getLocation(world: World?) = Location(world, x.toDouble(), y.toDouble(), z.toDouble())
